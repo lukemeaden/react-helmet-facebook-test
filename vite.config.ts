fb4efeba239ce0e04257507334a7d0849fd1341c
lukemeaden <lukemeaden@gmail.com>
2024-12-10 17:14:35 +0000

    Move dist to docs for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: './docs',
+    emptyOutDir: true,
+  },
   plugins: [react()],
 })
